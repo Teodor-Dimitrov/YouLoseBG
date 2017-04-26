9111aec2c185ff9a0555a1991a9a9234c1acfcd2
itkaramana <krmn1@abv.bg>
2017-04-27 02:48:13 +0300

    fixed methods in videoDao

--- YouLose/src/main/java/com/youlose/dao/VideoDAO.java
+++ YouLose/src/main/java/com/youlose/dao/VideoDAO.java
@@ -4,7 +4,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import com.youlose.model.Comment;
 import com.youlose.model.Playlist;
@@ -16,7 +20,6 @@ public class VideoDAO {
 	private static VideoDAO instance;
 	private static final HashMap<String, Video> allVideos = new HashMap<>();//
 
-
 	public synchronized VideoDAO getInstance() {
 		if (instance == null) {
 			instance = new VideoDAO();
@@ -26,22 +29,21 @@ public class VideoDAO {
 
 	public HashMap<String, Video> getAllVideos() throws SQLException {
 		String sql = "SELECT videos_id, name, path, views,date,description FROM videos;";
+		PreparedStatement st = null;
 		if (allVideos.isEmpty()) {
-			PreparedStatement st;
-		
-				st = DBManager.getInstance().getConnection().prepareStatement(sql);
-				ResultSet res = st.executeQuery();
-				while (res.next()) {
-					Video video = new Video();
-					video.setId(res.getInt("videos_id"));
-					video.setName(res.getString("name"));
-					video.setPath(res.getString("path"));
-					video.setViews(res.getInt("views"));
-					video.setDate(res.getTimestamp("date").toLocalDateTime());
-					video.setDescription(res.getString("description"));
-					allVideos.put(video.getName(), video);
-				}
-		
+			st = DBManager.getInstance().getConnection().prepareStatement(sql);
+			ResultSet res = st.executeQuery();
+			while (res.next()) {
+				Video video = new Video();
+				video.setId(res.getInt("videos_id"));
+				video.setName(res.getString("name"));
+				video.setPath(res.getString("path"));
+				video.setViews(res.getInt("views"));
+				video.setDate(res.getTimestamp("date").toLocalDateTime());
+				video.setDescription(res.getString("description"));
+				allVideos.put(video.getName(), video);
+			}
+
 		}
 		return allVideos;
 	}
@@ -49,145 +51,140 @@ public class VideoDAO {
 	public HashMap<String, Video> searchAllByString(String partOfName) throws SQLException {
 		String sql = "SELECT videos_id, name, path, views,date,description FROM videos WHERE name " + "like '"
 				+ partOfName + "';";
+		PreparedStatement st = null;
 		if (allVideos.isEmpty()) {
-			PreparedStatement st;
-		
-				st = DBManager.getInstance().getConnection().prepareStatement(sql);
-				ResultSet res = st.executeQuery();
-				while (res.next()) {
-					Video video = new Video();
-					video.setId(res.getInt("videos_id"));
-					video.setName(res.getString("name"));
-					video.setPath(res.getString("path"));
-					video.setViews(res.getInt("views"));
-					video.setDate(res.getTimestamp("date").toLocalDateTime());
-					video.setDescription(res.getString("description"));
-					allVideos.put(video.getName(), video);
-				}
+			st = DBManager.getInstance().getConnection().prepareStatement(sql);
+			ResultSet res = st.executeQuery();
+			while (res.next()) {
+				Video video = new Video();
+				video.setId(res.getInt("videos_id"));
+				video.setName(res.getString("name"));
+				video.setPath(res.getString("path"));
+				video.setViews(res.getInt("views"));
+				video.setDate(res.getTimestamp("date").toLocalDateTime());
+				video.setDescription(res.getString("description"));
+				allVideos.put(video.getName(), video);
+			}
 
 		}
 		return allVideos;
 	}
 
-	public void addVideo(Video video, User user) throws SQLException {// TODO ADD PUBLISHER TO DB
-													// ???
-		Connection con = DBManager.getInstance().getConnection();
-		PreparedStatement st;
-		
-			st = con.prepareStatement("INSERT INTO videos (name, path, views,date,description) VALUES (?,?,?,?,?);");
-			st.setString(1, video.getName());
-			st.setString(2, video.getName());
-			st.setInt(3, video.getViews());
-			// TODO st.setTimestamp(4, video.getDate());
-			// st.setPublisher(.....,video.getPublisher());
-			st.setString(5, video.getDescription());
-			st.executeUpdate();
-		
+	public synchronized void addVideo(Video video, User user) throws SQLException {
+		String sql = "INSERT INTO videos (name, path, views,date,description) VALUES (?,?,?,?,?);";
+		Instant instant = Instant.now();
+		Timestamp time = java.sql.Timestamp.from(instant);
 
-	}
-
-	public void likeVideo(Video video, User user) throws SQLException {
-		Connection con = DBManager.getInstance().getConnection();
-		PreparedStatement st;
-	
-			st = con.prepareStatement(
-					"INSERT INTO video_liked_or_disliked (users_user_id, videos_videos_id,like_or_dislike) VALUES (?,?,?);");
-			st.setInt(1, video.getId());
-			st.setInt(2, user.getUserID());
-			// TODO LIKES OR DISLIKES ?? LIKES -1 DISLIKES -2
-			st.setInt(3, 1);
-			st.executeUpdate();
-		
-	}
-
-	public void dislikeVideo(Video video, User user) throws SQLException {
-		Connection con = DBManager.getInstance().getConnection();
-		PreparedStatement st;
-	
-			st = con.prepareStatement(
-					"INSERT INTO video_liked_or_disliked (users_user_id, videos_videos_id,like_or_dislike) VALUES (?,?,?);");
-			st.setInt(1, video.getId());
-			st.setInt(2, user.getUserID());
-			// TODO LIKES OR DISLIKES ?? LIKES -1 DISLIKES -2
-			st.setInt(3, 2);
-			st.executeUpdate();
-		
-
-	}
-
-	public void removeLikeFromVideo(Video video, User user) throws SQLException {
-		Connection con = DBManager.getInstance().getConnection();
-		PreparedStatement st;
-		
-			st = con.prepareStatement("DELETE FROM video_liked_or_disliked WHERE users_user_id=" + user.getUserID()
-					+ " AND videos_videos_id=" + video.getId() + " AND like_or_dislike=1 ;");
-			st.executeUpdate();
-		
-	}
-
-	public void removeDislikeFromVideo(Video video, User user) {
-		Connection con = DBManager.getInstance().getConnection();
-		PreparedStatement st;
-		try {
-			st = con.prepareStatement("DELETE FROM video_liked_or_disliked WHERE users_user_id=" + user.getUserID()
-					+ " AND videos_videos_id=" + video.getId() + " AND like_or_dislike=2 ;");
-			st.executeUpdate();
-		} catch (SQLException e) {
-			System.out.println(" NOT deleted dislike");
-			e.printStackTrace();
+		PreparedStatement ps = null;
+		ps = DBManager.getInstance().getConnection().prepareStatement(sql);
+		ps.setString(1, video.getName());
+		ps.setString(2, video.getName());
+		ps.setInt(3, video.getViews());
+		ps.setTimestamp(4, time);
+		ps.setString(5, video.getDescription());
+		int rows = ps.executeUpdate();
+		if (rows > 0) {
+			System.out.println("vutre e");
 		}
+
+	}
+
+	public synchronized void likeVideo(Video video, User user) throws SQLException {
+		String sql = "INSERT INTO video_liked_or_disliked (users_user_id, videos_videos_id,like_or_dislike) VALUES (?,?,?);";
+		PreparedStatement ps = null;
+
+		ps = DBManager.getInstance().getConnection().prepareStatement(sql);
+		ps.setInt(1, video.getId());
+		ps.setInt(2, user.getUserID());
+		ps.setInt(3, 1);
+		int rows = ps.executeUpdate();
+		if (rows > 0) {
+			System.out.println("laiknato e");
+		}
+
+	}
+
+	public synchronized void dislikeVideo(Video video, User user) throws SQLException {
+		String sql = "INSERT INTO video_liked_or_disliked (users_user_id, videos_videos_id,like_or_dislike) VALUES (?,?,?);";
+		PreparedStatement ps = null;
+
+		ps = DBManager.getInstance().getConnection().prepareStatement(sql);
+		ps.setInt(1, video.getId());
+		ps.setInt(2, user.getUserID());
+		ps.setInt(3, 2);
+		int rows = ps.executeUpdate();
+		if (rows > 0) {
+			System.out.println("dislaiknato e");
+		}
+
+	}
+
+	public synchronized void removeLikeFromVideo(Video video, User user) throws SQLException {
+
+		String sql = "DELETE FROM video_liked_or_disliked WHERE users_user_id=? AND videos_videos_id=? AND like_or_dislike=1 ;";
+		PreparedStatement ps = null;
+
+		ps = DBManager.getInstance().getConnection().prepareStatement(sql);
+		ps.setInt(1, user.getUserID());
+		ps.setInt(2, video.getId());
+		int rows = ps.executeUpdate();
+		if (rows > 0) {
+			System.out.println("mahnat e like-a");
+		}
+
+	}
+
+	public synchronized void removeDislikeFromVideo(Video video, User user) throws SQLException {
+		String sql = "DELETE FROM video_liked_or_disliked WHERE users_user_id=? AND videos_videos_id=? AND like_or_dislike=2 ;";
+		PreparedStatement ps = null;
+
+		ps = DBManager.getInstance().getConnection().prepareStatement(sql);
+		ps.setInt(1, user.getUserID());
+		ps.setInt(2, video.getId());
+		int rows = ps.executeUpdate();
+		if (rows > 0) {
+			System.out.println("mahnat e dislike-a");
+		}
+
 	}
 
 	public void deleteVideo(Video video, User user) throws SQLException {
 		if (user.getUserID() == video.getPublisher()) {
-			Connection con = DBManager.getInstance().getConnection();
-			PreparedStatement st;
-				st = con.prepareStatement("DELETE FROM videos WHERE videos_id=" + video.getId() + ";");
-				st.executeUpdate();
+			String sql = "DELETE FROM videos WHERE videos_id=?;";
+			PreparedStatement ps = null;
+			ps = DBManager.getInstance().getConnection().prepareStatement(sql);
+			ps.setInt(1, video.getId());
+			int rows = ps.executeUpdate();
+			if (rows > 0) {
+				for (Entry<String, Video> entry : allVideos.entrySet()) {
+					if (entry.getValue().equals(video)) {
+						allVideos.remove(entry.getKey(), video);
+					}
+					
+				}
+				System.out.println("iztrih go");
+			}
 		}
 	}
 
-	public void addVideoToPlayList(Video video, Playlist playlist) throws SQLException {
-		Connection con = DBManager.getInstance().getConnection();
-		PreparedStatement st;
-		
-			st = con.prepareStatement(
-					"INSERT INTO playlist_videos (videos_videos_id,playlists_playlist_id) VALUES (?,?);");
-			st.setInt(1, video.getId());
-			st.setInt(2, playlist.getPlaylistID());
-			st.executeUpdate();
-	
-	}
-
-	public void removeVideoFromPlaylist(Video video, Playlist playlist) throws SQLException {
-		Connection con = DBManager.getInstance().getConnection();
-		PreparedStatement st;
-		
-			st = con.prepareStatement("DELETE FROM playlist_videos WHERE videos_videos_id= " + video.getId()
-					+ " playlists_playlist_id=" + playlist.getPlaylistID() + ";");
-
-			st.executeUpdate();
-		
-	}
 
 	public void viewVideo(Video video) throws SQLException {
 		String sql = "SELECT views FROM videos WHERE videos_id=" + video.getId() + ";";
 		if (allVideos.isEmpty()) {
 			PreparedStatement st = DBManager.getInstance().getConnection().prepareStatement(sql);
-				ResultSet res = st.executeQuery();
-				while (res.next()) {
-					video.setId((res.getInt("videos_id") + 1));
-				}
-			
+			ResultSet res = st.executeQuery();
+			while (res.next()) {
+				video.setId((res.getInt("videos_id") + 1));
+			}
+
 		}
 	}
-	
-	
-	
-	public void deleteComment(Comment comment) throws SQLException{
+
+	public void deleteComment(Comment comment) throws SQLException {
 		Connection con = DBManager.getInstance().getConnection();
-		PreparedStatement st = con.prepareStatement("DELETE FROM comments WHERE comment_id= " +comment.getCommentID()+ ";");
-			st.executeUpdate();
-		
+		PreparedStatement st = con
+				.prepareStatement("DELETE FROM comments WHERE comment_id= " + comment.getCommentID() + ";");
+		st.executeUpdate();
+
 	}
 }
